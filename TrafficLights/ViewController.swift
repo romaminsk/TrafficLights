ae773d024d12b551c6252136dd3266e8228b21bb
Raman Novakhatski <romabelarus@mail.ru>
2022-01-14 23:41:37 +0300

    create traffic light view controller

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -11,9 +11,56 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        roundView(view: redView)
+        roundView(view: yellowView)
+        roundView(view: greenView)
+    }
+    
+    var timer: Timer?
+
+    @IBOutlet weak var redView: UIView!
+
+    @IBOutlet weak var yellowView: UIView!
+
+    @IBOutlet weak var greenView: UIView!
+
+    @IBAction func actionView(_ sender: UIButton) {
+        
+        let views = [redView, yellowView, greenView]
+        var counter = 0
+        
+        self.yellowView.alpha = 0.3
+        self.yellowView.alpha = 0.3
+        self.greenView.alpha = 0.3
+
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { (timer) in
+            
+            if counter == 0 {
+                self.redView.alpha = 1
+                self.yellowView.alpha = 0.3
+                self.greenView.alpha = 0.3
+            } else if counter == 1 {
+                self.yellowView.alpha = 1
+                self.redView.alpha = 0.3
+                self.greenView.alpha = 0.3
+            } else if counter == 2 {
+                self.greenView.alpha = 1
+                self.redView.alpha = 0.3
+                self.yellowView.alpha = 0.3
+            }
+
+            counter += 1
+            
+            if counter > views.count {
+                counter = 0
+            }
+        })
     }
 
-
+    func roundView(view: UIView) {
+        view.layer.cornerRadius = min(view.frame.size.height, view.frame.size.width) / 2.0
+        view.clipsToBounds = true
+    }
 }
 
